2abfe2ad586387fa3acc5c07b4dc070ac30d79f1
SashTAl8 <99807832+SashTAl8@users.noreply.github.com>
2022-04-01 08:38:59 -0500

    Register, Query and Connect Database to web application

--- front-end/src/components/Register.js
+++ front-end/src/components/Register.js
@@ -1,72 +1,155 @@
+import React, { useContext, useState } from 'react'
 import '../css/Register.css'
+
+import axios from 'axios';
+import AppContext from './AppContext';
+import { useNavigate } from 'react-router';
 
 export default function Register() {
 
+	const { dispatch } = useContext(AppContext);
 
-    return (
-        <div id="hero" className="container has-two-col">
+	const [userInput, setUserInput] = useState({
+		name: "",
+		email: "",
+		password: ""
+	});
 
-            <div id="register">
+	const [errorMessage, setErrorMessage] = useState(null);
 
-                <form id="register-form">
+	const navigate = useNavigate();
 
-                    <div id="form-name">
-                        <h2>Register Form</h2>
-                    </div>
+	const [passwordShown, setPasswordShown] = useState(false)
 
-                    <div id="id-password-submit">
+	const togglePassword = () => {
+		setPasswordShown(!passwordShown)
+	}
+
+	const onChangeHandler = (e) => {
+
+		//when user type Name, Email, password
+		// setUserInput will update value into userInput
+		setUserInput({
+			...userInput,
+			[e.target.name]: e.target.value
+		})
+	}
+
+	const onSubmitHandler = async (e) => {
+		try {
+			e.preventDefault(); // current Page will not reload when submit
+
+			const options = {
+				method: "post",
+				url: "/api/v1/auth/register",
+				data: userInput // data is name, email, password from the form
+			};
+
+			// send request to server 
+			const response = await axios(options);
+
+			// get TOKEN and userName from the response
+			const { token, userName } = response.data.data;
+
+			// save TOKEN into Local Storage
+			localStorage.setItem("token", token);
+
+			// use DISPATCH to update the initital State with userName from the server
+			dispatch({
+				type: "CURRENT_USER",
+				payload: { userName }
+			});
+
+			navigate("/");
+
+			dispatch({
+				type: "CURRENT_USER",
+				payload: null
+			})
+
+			alert("succesfully registered, Please login to continue");
+		} catch (error) {
+			setErrorMessage(error.response.data.message);
+		}
+	}
+
+	return (
+		<div id="hero" className="container has-two-col">
+
+			<div id="register">
+
+				<form id="register-form" onSubmit={onSubmitHandler}>
+
+					<div id="form-name">
+						<h2>Register Form</h2>
+					</div>
+
+					<div id="id-password-submit">
+
+						{errorMessage &&
+							(Array.isArray(errorMessage) ? ( // check if errorMessage is an array?
+								errorMessage.map((err) => (
+									<div className="error-message">
+										Error: {err}
+									</div>
+								))
+							) : (
+								<div className="error-message">
+									Error: {errorMessage}
+								</div>
+							))
+						}
+
+						<div id="full-name">
+							<input
+								type="text"
+								name="name"
+								placeholder="Full Name"
+								value={userInput.name}
+								onChange={onChangeHandler}
+
+							/>
+						</div>
+
+						<div id="email">
+							<input
+								type="email"
+								name="email"
+								placeholder="Email"
+								value={userInput.email}
+								onChange={onChangeHandler}
+							/>
+						</div>
+
+						<div id="password">
+							<input
+								type={passwordShown ? "text" : "password"}
+								name="password"
+								placeholder="Password"
+								value={userInput.password}
+								onChange={onChangeHandler}
+							>
+							</input>
+
+							<span id="show-password" onClick={togglePassword}>Show</span>
+
+						</div>
+
+						<div id="register">
+							<button type="submit"> Create Account</button>
+						</div>
+
+					</div>
+
+				</form>
 
 
-                        <div className="error-message">
-                            Error:...
-                        </div>
+			</div>
 
-                        <div id="full-name">
-                            <input
-                                type="text"
-                                name="name"
-                                placeholder="Full Name"
+			<div id="hero-right">
+				<img src="image/background1/people.png" alt='' />
+			</div>
 
-                            />
-                        </div>
-
-                        <div id="email">
-                            <input
-                                type="email"
-                                name="email"
-                                placeholder="Email"
-
-                            />
-                        </div>
-
-                        <div id="password">
-                            <input
-                                type="password"
-                                name="password"
-                                placeholder="Password"
-
-                            >
-                            </input>
-
-                            <span id="show-password">Show</span>
-
-                        </div>
-
-                        <div id="register">
-                            <button type="submit"> Create Account</button>
-                        </div>
-
-                    </div>
-
-                </form>
-
-
-            </div>
-
-            <div id="hero-right">
-                <img src="image/background1/people.png" alt='' />
-            </div>
-
-        </div>
-    )
+		</div>
+	)
 }
--- front-end/src/reducers/AppReducer.js
+++ front-end/src/reducers/AppReducer.js
@@ -1,3 +1,12 @@
 export default function reducer(state, action) {
-    
+    switch (action.type) {
+        case "CURRENT_USER":
+            return {
+                ...state,
+                user: action.payload
+            };
+        default:
+            return state;
+
+    }
 }
