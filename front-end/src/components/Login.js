4ec8cb556682b086ff8d07e23119b6ec8045f566
Khai Luong <94124188+kevinluong112@users.noreply.github.com>
2022-04-02 23:10:26 -0500

    Login  Query and Connect Database to web application

--- front-end/src/components/Login.js
+++ front-end/src/components/Login.js
@@ -1,15 +1,97 @@
+import React, { useContext, useState } from 'react'
 import '../css/Login.css'
 import { Link } from 'react-router-dom';
+import axios from 'axios';
+import AppContext from './AppContext';
+import { useNavigate } from 'react-router';
 
 
 export default function Login() {
+
+    // useContext: to get the DISPATCH from AppContext
+    const { dispatch } = useContext(AppContext);
+
+    const [userInput, setUserInput] = useState({
+        email: "",
+        password: ""
+    });
+
+    const [passwordShown, setPasswordShown] = useState(false)
+
+    const togglePassword = () => {
+        setPasswordShown(!passwordShown)
+    }
+
+
+    const [errorMessage, setErrorMessage] = useState(null);
+
+    const navigate = useNavigate();
+
+    const onChangeHandler = (e) => {
+
+        //when user type Email, password
+        // setUserInput will update value into userInput
+        setUserInput({
+            ...userInput,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const onSubmitHandler = async (e) => {
+        try {
+            e.preventDefault(); // current Page will not reload when submit
+
+            const options = {
+                method: "post",
+                url: "/api/v1/auth/login",
+                data: userInput // data is email, password from the form
+            };
+
+            // send request to server 
+            const response = await axios(options);
+
+            const {
+                token,
+                userId,
+                name,
+                address1,
+                address2,
+                city,
+                state,
+                zipcode
+            } = response.data.data;
+
+            // save TOKEN into Local Storage
+            localStorage.setItem("token", token);
+
+            // use DISPATCH to update the initital State with userName from the server
+            dispatch({
+                type: "CURRENT_USER",
+                payload: {
+                    userId,
+                    name,
+                    address1,
+                    address2,
+                    city,
+                    state,
+                    zipcode
+                }
+            });
+
+            navigate("/");
+
+
+        } catch (error) {
+            setErrorMessage(error.response.data.message);
+        }
+    }
 
     return (
         <div id="hero" className="container has-two-col">
 
             <div id="login">
 
-                <form id="login-form" >
+                <form id="login-form" onSubmit={onSubmitHandler}>
 
                     <div id="form-name">
                         <h2>Login Form</h2>
@@ -18,11 +100,11 @@ export default function Login() {
 
                     <div id="id-password-submit">
 
-
-                        <div id="error-massage">
-                            Error:
-                        </div>
-
+                        {errorMessage && (
+                            <div id="error-massage">
+                                Error: {errorMessage}
+                            </div>
+                        )}
 
 
                         <div id="email">
@@ -31,16 +113,21 @@ export default function Login() {
                                 name="email"
                                 placeholder="Email"
                                 required
+                                value={userInput.email}
+                                onChange={onChangeHandler}
                             />
                         </div>
 
                         <div id="password">
                             <input
+                                type={passwordShown ? "text" : "password"}
                                 name="password"
                                 placeholder="Password"
                                 required
+                                value={userInput.password}
+                                onChange={onChangeHandler}
                             />
-                            <span id="show-password">Show</span>
+                            <span id="show-password" onClick={togglePassword}>Show</span>
                         </div>
 
                         <div id="signin">
