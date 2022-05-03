c86531df3239249c60e4aaab7bcb16174649316c
Thanh Vu <65506586+vvt4994@users.noreply.github.com>
2022-05-03 09:48:00 -0500

    Validation update

--- front-end/src/components/Register.js
+++ front-end/src/components/Register.js
@@ -1,155 +1,137 @@
-import React, { useContext, useState } from 'react'
-import '../css/Register.css'
+import React, { useContext, useState } from "react";
+import "../css/Register.css";
 
-import axios from 'axios';
-import AppContext from './AppContext';
-import { useNavigate } from 'react-router';
+import axios from "axios";
+import AppContext from "./AppContext";
+import { useNavigate } from "react-router";
 
 export default function Register() {
+  const navigate = useNavigate();
+  const { dispatch } = useContext(AppContext);
 
-	const { dispatch } = useContext(AppContext);
+  const [userInput, setUserInput] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
 
-	const [userInput, setUserInput] = useState({
-		name: "",
-		email: "",
-		password: ""
-	});
+  const [errorMessage, setErrorMessage] = useState(null);
 
-	const [errorMessage, setErrorMessage] = useState(null);
+  const [passwordShown, setPasswordShown] = useState(false);
 
-	const navigate = useNavigate();
+  const togglePassword = () => {
+    setPasswordShown(!passwordShown);
+  };
 
-	const [passwordShown, setPasswordShown] = useState(false)
+  const onChangeHandler = (e) => {
+    //when user type Name, Email, password
+    // setUserInput will update value into userInput
+    setUserInput({
+      ...userInput,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-	const togglePassword = () => {
-		setPasswordShown(!passwordShown)
-	}
+  const onSubmitHandler = async (e) => {
+    try {
+      e.preventDefault(); // current Page will not reload when submit
 
-	const onChangeHandler = (e) => {
+      const options = {
+        method: "post",
+        url: "/api/v1/auth/register",
+        data: userInput, // data is name, email, password from the form
+      };
 
-		//when user type Name, Email, password
-		// setUserInput will update value into userInput
-		setUserInput({
-			...userInput,
-			[e.target.name]: e.target.value
-		})
-	}
+      // send request to server
+      const response = await axios(options);
 
-	const onSubmitHandler = async (e) => {
-		try {
-			e.preventDefault(); // current Page will not reload when submit
+      // get TOKEN and userName from the response
+      const { token, userName } = response.data.data;
 
-			const options = {
-				method: "post",
-				url: "/api/v1/auth/register",
-				data: userInput // data is name, email, password from the form
-			};
+      // save TOKEN into Local Storage
+      localStorage.setItem("token", token);
 
-			// send request to server 
-			const response = await axios(options);
+      // use DISPATCH to update the initital State with userName from the server
+      dispatch({
+        type: "CURRENT_USER",
+        payload: { userName },
+      });
 
-			// get TOKEN and userName from the response
-			const { token, userName } = response.data.data;
+      navigate("/");
 
-			// save TOKEN into Local Storage
-			localStorage.setItem("token", token);
+      dispatch({
+        type: "CURRENT_USER",
+        payload: null,
+      });
 
-			// use DISPATCH to update the initital State with userName from the server
-			dispatch({
-				type: "CURRENT_USER",
-				payload: { userName }
-			});
+      alert("succesfully registered, Please login to continue");
+    } catch (error) {
+      setErrorMessage(error.response.data.message);
+    }
+  };
 
-			navigate("/");
+  return (
+    <div id="hero" className="container has-two-col">
+      <div id="register">
+        <form id="register-form" onSubmit={onSubmitHandler}>
+          <div id="form-name">
+            <h2>Register Form</h2>
+          </div>
 
-			dispatch({
-				type: "CURRENT_USER",
-				payload: null
-			})
+          <div id="id-password-submit">
+            {errorMessage &&
+              (Array.isArray(errorMessage) ? ( // check if errorMessage is an array?
+                errorMessage.map((err) => (
+                  <div className="error-message">Error: {err}</div>
+                ))
+              ) : (
+                <div className="error-message">Error: {errorMessage}</div>
+              ))}
 
-			alert("succesfully registered, Please login to continue");
-		} catch (error) {
-			setErrorMessage(error.response.data.message);
-		}
-	}
+            <div id="full-name">
+              <input
+                type="text"
+                name="name"
+                placeholder="Full Name"
+                value={userInput.name}
+                onChange={onChangeHandler}
+              />
+            </div>
 
-	return (
-		<div id="hero" className="container has-two-col">
+            <div id="email">
+              <input
+                type="email"
+                name="email"
+                placeholder="Email"
+                value={userInput.email}
+                onChange={onChangeHandler}
+              />
+            </div>
 
-			<div id="register">
+            <div id="password">
+              <input
+                type={passwordShown ? "text" : "password"}
+                name="password"
+                placeholder="Password"
+                value={userInput.password}
+                onChange={onChangeHandler}
+              ></input>
 
-				<form id="register-form" onSubmit={onSubmitHandler}>
+              <span id="show-password" onClick={togglePassword}>
+                Show
+              </span>
+            </div>
 
-					<div id="form-name">
-						<h2>Register Form</h2>
-					</div>
+            <div id="register">
+              <button type="submit"> Create Account</button>
+            </div>
+          </div>
+        </form>
+      </div>
 
-					<div id="id-password-submit">
-
-						{errorMessage &&
-							(Array.isArray(errorMessage) ? ( // check if errorMessage is an array?
-								errorMessage.map((err) => (
-									<div className="error-message">
-										Error: {err}
-									</div>
-								))
-							) : (
-								<div className="error-message">
-									Error: {errorMessage}
-								</div>
-							))
-						}
-
-						<div id="full-name">
-							<input
-								type="text"
-								name="name"
-								placeholder="Full Name"
-								value={userInput.name}
-								onChange={onChangeHandler}
-
-							/>
-						</div>
-
-						<div id="email">
-							<input
-								type="email"
-								name="email"
-								placeholder="Email"
-								value={userInput.email}
-								onChange={onChangeHandler}
-							/>
-						</div>
-
-						<div id="password">
-							<input
-								type={passwordShown ? "text" : "password"}
-								name="password"
-								placeholder="Password"
-								value={userInput.password}
-								onChange={onChangeHandler}
-							>
-							</input>
-
-							<span id="show-password" onClick={togglePassword}>Show</span>
-
-						</div>
-
-						<div id="register">
-							<button type="submit"> Create Account</button>
-						</div>
-
-					</div>
-
-				</form>
-
-
-			</div>
-
-			<div id="hero-right">
-				<img src="image/background1/people.png" alt='' />
-			</div>
-
-		</div>
-	)
+      <div id="hero-right">
+        <img src="image/background1/people.png" alt="" />
+      </div>
+    </div>
+  );
 }
